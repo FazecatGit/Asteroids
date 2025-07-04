d0389dc08d1dcf082ee2216969ca75510cb49b62
Fazecat <peterisnot99@gmail.com>
2025-07-04 14:42:47 +0100

    B: made game open in terminal

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import pygame, constants
+import pygame
+from constants import *
+
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
-    print(constants.SCREEN_WIDTH)
-    print(constants.SCREEN_HEIGHT)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        
+    
+
+
 
 if __name__ == "__main__":
     main()
