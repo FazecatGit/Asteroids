e88da1f2fd99185714df6a7ff03e74722b6c4c88
Fazecat <peterisnot99@gmail.com>
2025-07-04 13:54:54 +0100

    asteroids import

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame, constants
+
+def main():
+    print("Starting Asteroids!")
+    print(constants.SCREEN_WIDTH)
+    print(constants.SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
